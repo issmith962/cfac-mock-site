39ac82191315ce4156111661b23c1d063cc198f4
Isaac Smith <is.smith.962@gmail.com>
2021-07-24 21:06:44 -0600

    Finished up audition page form submission (js, etc). Also added photo creds after submitting a form

---
+++ scripts/audition-script.js
@@ -0,0 +1,28 @@
+document.getElementById("submitBox").addEventListener("click", function(event) {
+	let allAreFilled = true;
+	document.getElementById("auditionForm").querySelectorAll("[required]").forEach(function(i) {
+		if (!allAreFilled) return;
+		if (!i.value) allAreFilled = false;
+		if (i.type === "radio") {
+			let radioValueCheck = false;
+			document.getElementById("auditionForm").querySelectorAll(`[name=${i.name}]`).forEach(function(r) {
+				if (r.checked) radioValueCheck = true;
+			})
+			allAreFilled = radioValueCheck;
+		}
+	})
+	if (!allAreFilled) {
+		alert("Please fill all the fields");
+	} else {
+		event.preventDefault(); 
+		document.getElementById("content").style.display = "none";
+		document.getElementById("submitted").style.display = "flex";
+	}
+}); 
+
+
+
+
+
+
+	 
